d17e2877f465eb087e9ff64d829f7a5847fc9700
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-09-22 23:46:57 +0530

    sharing example for passing data across components using context

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import WithCounterTwo from './components/WithCounterTwo';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/UserContext';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <WithCounterTwo>
-          { (count, incrementCounter) => (
-          <ClickCounterTwo count = { count } incrementCounter = { incrementCounter } />
-          )}
-        </WithCounterTwo>
-        <WithCounterTwo>
-          { (count, incrementCounter) => (
-          <HoverCounterTwo count = { count } incrementCounter = { incrementCounter } />
-          )}
-        </WithCounterTwo>
+        <UserProvider value = 'Harsha'>
+          <ComponentC />
+        </UserProvider>
       </div>
     );
   }
